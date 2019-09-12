a21c02ab1da1875aee8f64b1ba3ed5e64379e745
matthinrichsen <hinrichsen.matthew@gmail.com>
2019-09-11 23:12:39 -0500

    Use correct filepath.Split - and fix build

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func fixDirectory(path string) {
 			return nil
 		}
 
-		if filepath.Dir(directory) == `vendor` {
+		if _, folderName := filepath.Split(directory); folderName == `vendor` {
 			return filepath.SkipDir
 		}
 
@@ -114,7 +114,7 @@ func buildOutImports(files map[string]*ast.File, fileSet *token.FileSet, sn util
 				continue
 			}
 
-			info, nextRoundOfFiles, err := compile(filepath.Join(os.Getenv("GOPATH"), "src", removeQuotes(i.Path.Value)), fileSet)
+			info, nextRoundOfFiles, err := compile(filepath.Join(os.Getenv("GOPATH"), "src", util.RemoveQuotes(i.Path.Value)), fileSet)
 			if err == nil {
 				sn.AddPackage(i.Path.Value, info)
 				buildOutImports(nextRoundOfFiles, fileSet, sn)
