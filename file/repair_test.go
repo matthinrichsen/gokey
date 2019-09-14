85e106217a5e4f247b4dd65a51259958b43d75b4
matthinrichsen <hinrichsen.matthew@gmail.com>
2019-09-13 21:46:03 -0500

    Test complext struct

--- file/repair_test.go
+++ file/repair_test.go
@@ -45,3 +45,53 @@ var abc = LastStruct{Int: 1}
 `
 	assert.Equal(t, expectedString, b.String())
 }
+
+func TestFileRepair_ComplexStruct(t *testing.T) {
+	file := `package anotherPackage
+
+type StructOne struct {
+	Name string
+	A	NestedStruct
+}
+type NestedStruct struct {
+	One	int
+	Two	int
+	Three	int
+}
+
+func NewStructOne() StructOne {
+	return StructOne{"ThisIsMyName", NestedStruct{1,2,3}}
+}
+`
+	fset := token.NewFileSet()
+	ast, err := parser.ParseFile(fset, `testfile.go`, []byte(file), parser.ParseComments)
+	require.NoError(t, err)
+
+	sm := util.NewStructManager()
+	info, err := util.CompileFiles(`tests`, fset, ast)
+	require.NoError(t, err)
+
+	sm.AddPackage(`github.com/matthinrichsen/anotherPackage`, info)
+	assert.True(t, Repair(ast, `github.com/matthinrichsen/anotherPackage`, sm))
+
+	b := &bytes.Buffer{}
+	printer.Fprint(b, fset, ast)
+
+	expectedString := `package anotherPackage
+
+type StructOne struct {
+	Name	string
+	A	NestedStruct
+}
+type NestedStruct struct {
+	One	int
+	Two	int
+	Three	int
+}
+
+func NewStructOne() StructOne {
+	return StructOne{Name: "ThisIsMyName", A: NestedStruct{One: 1, Two: 2, Three: 3}}
+}
+`
+	assert.Equal(t, expectedString, b.String())
+}
